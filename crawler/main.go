6718cad7914dde09d3ca9ea6a9fdb616971dceaa
xipper <864353360@qq.com>
2022-11-21 15:20:41 +0800

    add zhenai

--- crawler/main.go
+++ crawler/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	//e := engine.ConcurrentEngine{
+	//	Scheduler:   &scheduler.QueuedScheduler{},
+	//	WorkerCount: 100,
+	//}
+	//
+	//e.Run(engine.Request{
+	//	Url:        "http://www.zhenai.com/zhenghun",
+	//	ParserFunc: parser.ParseCityList,
+	//})
+	i, err := strconv.Atoi("-42")
+	if err != nil {
+		fmt.Println(i)
+	}
 }
