4b2faf49a5303d4192587e4de0041801a291b5c9
xipper <864353360@qq.com>
2022-11-21 15:20:41 +0800

    add zhenai

--- crawler/main.go
+++ crawler/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"go-reptile/crawler/engine"
+	"go-reptile/crawler/scheduler"
+	"go-reptile/crawler/zhenai/parser"
+)
 
+func main() {
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.QueuedScheduler{},
+		WorkerCount: 100,
+	}
+
+	e.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
